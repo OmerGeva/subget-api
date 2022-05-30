f3c5a84066d4d413beb6e23fe74421c645a14a88
Omer Geva <ogeva97@gmail.com>
2022-05-30 18:04:10 +0300

    gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,7 @@ console.log(process.env.STAGE);
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
           username: configService.get('DB_USER'),
+          user: configService.get('DB_USER'),
           password: configService.get('DB_PASSWORD'),
           database: configService.get('DB_DATABASE'),
           autoLoadEntities: true,
